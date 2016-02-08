f0f78c1e7d33cd055fcbcb79944f596b4270fffe
Otonye Bestman <oerebestman@yahoo.com>
2016-02-08 14:42:13 +0000

    Impllemented quick sort
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@34 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

---
+++ src/d2/teamproject/module/algorithm/sorting/QuickSort.java
@@ -0,0 +1,50 @@
+package d2.teamproject.module.algorithm.sorting;
+
+public class QuickSort {
+	 private int array[];
+	 private int length;
+	 public void sort(int[] inputArr) {
+         
+	        if (inputArr == null || inputArr.length == 0) {
+	            return;
+	        }
+	        this.array = inputArr;
+	        int length = inputArr.length;
+	        quickSort(0, length - 1);
+	    }
+	 
+	    private void quickSort(int lowerIndex, int higherIndex) {
+	         
+	        int i = lowerIndex;
+	        int j = higherIndex;
+	        // calculate pivot number, I am taking pivot as middle index number
+	        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+	        // Divide into two arrays
+	        while (i <= j) {
+	            
+	            while (array[i] < pivot) {
+	                i++;
+	            }
+	            while (array[j] > pivot) {
+	                j--;
+	            }
+	            if (i <= j) {
+	                exchangeNumbers(i, j);
+	                //move index to next position on both sides
+	                i++;
+	                j--;
+	            }
+	        }
+	        // call quickSort() method recursively
+	        if (lowerIndex < j)
+	            quickSort(lowerIndex, j);
+	        if (i < higherIndex)
+	            quickSort(i, higherIndex);
+	    }
+	 
+	    private void exchangeNumbers(int i, int j) {
+	        int temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	    }
+}
