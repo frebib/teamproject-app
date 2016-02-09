06b35e5818b92be270c2f8677456a7d45f95a0b8
Luke Taher <luketaher@gmail.com>
2016-02-09 18:38:22 +0000

    Fixed copy method in SearchStack so that it actually makes an actual copy in the actual order of the actual stack
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@36 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/module/algorithm/search/datastructures/SearchStack.java
+++ src/d2/teamproject/module/algorithm/search/datastructures/SearchStack.java
@@ -2,31 +2,42 @@ package d2.teamproject.module.algorithm.search.datastructures;
 
 import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * Stores a {@code Collection} of items in a Stack {@link d2.teamproject.module.algorithm.search.datastructures.DataStructure} The implementation actually uses a {@link java.util.ArrayDeque} in place of a {@link java.util.Stack}
+ * Stores a {@code Collection} of items in a Stack
+ * {@link d2.teamproject.module.algorithm.search.datastructures.DataStructure}
+ * The implementation actually uses a {@link java.util.ArrayDeque} in place of a
+ * {@link java.util.Stack}
  *
- * @param <A> @inheritDoc
+ * @param <A>
+ *            @inheritDoc
  */
 public class SearchStack<A> extends ArrayDeque<A> implements DataStructure<A> {
-    public SearchStack(Collection<? extends A> c) {
-        super(c);
-    }
-
-    @Override
-    public A getHead() {
-        return pop();
-    }
-
-    @Override
-    public boolean add(A a) {
-        push(a);
-        return true;
-    }
-    
-    @Override
-	public DataStructure<A> copy() {
-		return new SearchStack<A>(this);
+	public SearchStack(Collection<? extends A> c) {
+		super(c);
 	}
 
+	@Override
+	public A getHead() {
+		return pop();
+	}
+
+	@Override
+	public boolean add(A a) {
+		push(a);
+		return true;
+	}
+
+	@Override
+	public DataStructure<A> copy() {
+		SearchStack<A> temp = new SearchStack<A>(this);
+		SearchStack<A> temp2 = new SearchStack<A>(this);
+		temp.clear();
+		for (int i = this.size() - 1; i >= 0; i--) {
+			temp.add(temp2.getHead());
+		}
+		return temp;
+	}
 }
