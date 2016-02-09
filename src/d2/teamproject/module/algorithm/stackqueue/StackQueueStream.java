b331d011b10e9f429930e910b4c93ef5e734f8ea
Luke Taher <luketaher@gmail.com>
2016-02-09 13:01:32 +0000

    Implemented Stack and Queue stream
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@35 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

---
+++ src/d2/teamproject/module/algorithm/stackqueue/StackQueueStream.java
@@ -0,0 +1,60 @@
+package d2.teamproject.module.algorithm.stackqueue;
+
+import java.util.ArrayList;
+
+import d2.teamproject.module.algorithm.AlgoStream;
+import d2.teamproject.module.algorithm.search.datastructures.DataStructure;
+
+public class StackQueueStream<T, L extends DataStructure<T>> implements AlgoStream<L> {
+	
+	private L current;
+	private ArrayList<L> states; 
+	
+	public StackQueueStream(L sq){
+		this.current = sq;
+	}
+
+	@Override
+	public void initialise() {
+		states = new ArrayList<L>();
+		states.add(current);
+	}
+
+	@Override
+	public void reset() {
+		states.clear();
+		states.add(current);
+	}
+
+	@Override
+	public L getNext() {
+		L temp = (L) current.copy();
+		current.getHead();
+		L permCur = (L) current.copy();
+		states.add(permCur);
+		return (L) temp.copy();
+	}
+	
+	public void add(T item)
+	{
+		current.add(item);
+		states.add(current);
+	}
+
+	@Override
+	public L getPrevious() {
+		if(!hasPrevious())
+			return null;
+		return states.get(states.size() -2);
+	}
+
+	@Override
+	public boolean hasNext() {
+		return !current.isEmpty();
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		return states.size() > 0;
+	}
+}
