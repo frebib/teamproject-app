329359f394233a31e149e95ccd358f86d3272530
Joseph Groocock <frebib@gmail.com>
2016-02-10 01:04:47 +0000

    SearchState: Added Javadoc
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@44 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/module/algorithm/search/SearchState.java
+++ src/d2/teamproject/module/algorithm/search/SearchState.java
@@ -6,6 +6,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * A state to represent the state of search from {@link SearchStream}
+ * at any given point in time. Provides a look into all data that represents the state
+ * @param <E> Element that nodes in search represent
+ * @param <L> A frontier set that changes how the search is performed
+ */
 public class SearchState<E, L extends BaseDataStructure<Node<E>>> {
     private boolean isComplete = false;
     private final L frontier;
@@ -19,18 +25,31 @@ public class SearchState<E, L extends BaseDataStructure<Node<E>>> {
         this.isComplete = (path != null);
     }
 
+    /**
+     * Gets whether the path search has finished
+     */
     public boolean isComplete() {
         return isComplete;
     }
 
+    /**
+     * Gets the frontier {@link BaseDataStructure} from the search
+     */
     public L getFrontier() {
         return frontier;
     }
 
+    /**
+     * Gets the visited set from the search
+     */
     public Set<Node<E>> getVisited() {
         return visited;
     }
 
+    /**
+     * Gets the path found by the search.
+     * @return a path or null if {@code isComplete()} returns false
+     */
     public List<Node<E>> getPath() {
         return path;
     }
