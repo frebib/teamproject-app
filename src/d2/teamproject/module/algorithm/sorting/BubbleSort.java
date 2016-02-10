ba19b8164d941a2476b23d35213ea583d84fa9f0
Otonye Bestman <oerebestman@yahoo.com>
2016-02-10 15:26:52 +0000

    BubbleSort: Now works with generic types
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@48 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/module/algorithm/sorting/BubbleSort.java
+++ src/d2/teamproject/module/algorithm/sorting/BubbleSort.java
@@ -1,17 +1,41 @@
 package d2.teamproject.module.algorithm.sorting;
 
-public class BubbleSort{
-	public static int [] sort(int[] items){
-		for(int i = 0; i < items.length - 1; i++){
-			for(int k = items.length - 1; k > i; k--){
-				if(items[i] > items[k]){
-					int store = items[i];
-					items[i]  = items[k];
-					items[k] = store;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ *@author Otonye
+ *@class BubbleSort
+ */
+
+public class BubbleSort<E extends Comparable<E>>{
+	 /**
+	  * sort 
+	  * @param items
+	  * @return items
+	  */
+	public List<E> sort(List<E> items){
+		for(int i = 0; i < items.size() - 1; i++){
+			for(int k = items.size() - 1; k > i; k--){
+				if(items.get(i).compareTo(items.get(k)) > 0){
+					E store = items.get(i);
+					items.set(i, items.get(k));
+					items.set(k, store);
 				}
 			}
 		}
 		return items;
 	}
+	public static void main(String[] args) {
+        List<Integer> list = new Random().ints(20, 0, 100).boxed().collect(Collectors.toList());
+        System.out.println(list.stream().map(Object::toString).collect(Collectors.joining(", ")));
+        System.out.println(new BubbleSort<Integer>()
+        		.sort(list)
+        		.stream()
+        		.map(Object::toString)
+        		.collect(Collectors.joining(", ")));
+    }
 	
 }
