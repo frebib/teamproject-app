829fe3848d405ebd240a3c4222f017482c31e49f
Joseph Groocock <frebib@gmail.com>
2016-02-11 16:54:00 +0000

    TubeSearchModule: Add missing methods
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@60 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/module/tubesearch/TubeSearchModule.java
+++ src/d2/teamproject/module/tubesearch/TubeSearchModule.java
@@ -2,9 +2,25 @@ package d2.teamproject.module.tubesearch;
 
 import d2.teamproject.VisApp;
 import d2.teamproject.module.BaseModule;
+import javafx.scene.image.Image;
 
 public class TubeSearchModule implements BaseModule {
     @Override
     public void init(VisApp app) {
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDesc() {
+        return null;
+    }
+
+    @Override
+    public Image getBanner() {
+        return null;
     }
 }
