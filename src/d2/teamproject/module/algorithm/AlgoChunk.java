c2a9d0bb1c5b6074b06deec43511f0a7fbcb078b
Joseph Groocock <frebib@gmail.com>
2016-02-11 12:25:00 +0000

    Merge AlgoChunk into AlgoStream
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@52 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/module/algorithm/AlgoChunk.java
+++
@@ -1,8 +0,0 @@
-package d2.teamproject.module.algorithm;
-
-import java.util.List;
-
-public interface AlgoChunk<T> extends AlgoStream<T> {
-    T getNth(int n);
-    List<T> getAll();
-}
--- src/d2/teamproject/module/algorithm/AlgoStream.java
+++ src/d2/teamproject/module/algorithm/AlgoStream.java
@@ -1,10 +1,15 @@
 package d2.teamproject.module.algorithm;
 
+import java.util.List;
+
 public interface AlgoStream<T> {
     void initialise();
     void reset();
     T getNext();
     T getPrevious();
+    T getNth(int n);
     boolean hasNext();
     boolean hasPrevious();
+    boolean hasNth(int n);
+    List<T> getAll();
 }
