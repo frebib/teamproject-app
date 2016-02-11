f2ab056abe477f3f453119525225201ae1616ff4
Parth Chandratreya <pac442@bham.ac.uk>
2016-02-11 14:39:49 +0000

    Basic View implemented
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@56 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/gui/BasicView.java
+++ src/d2/teamproject/gui/BasicView.java
@@ -1,4 +1,61 @@
 package d2.teamproject.gui;
 
-public class BasicView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class BasicView extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("PARTH");
+        primaryStage.setMaximized(true);
+
+        StackPane pane = new StackPane();
+        BorderPane bp = new BorderPane();
+        bp.setPadding(new Insets(10, 20, 10, 20));
+
+        Button top = new Button("BACK");
+        HBox topBox = new HBox();
+        topBox.getChildren().addAll(top);
+
+        StackPane centerPane = new StackPane();
+        centerPane.setStyle("-fx-background-color: green");
+
+        Button bottomLeft = new Button("HELP");
+        Button bottomRight = new Button("TICK");
+        //HBox bottomBox = new HBox();
+        HBox bottomLeftBox = new HBox();
+        HBox bottomRightBox = new HBox();
+        StackPane bottomBox = new StackPane();
+
+        bottomLeftBox.getChildren().addAll(bottomLeft);
+        bottomLeftBox.alignmentProperty().setValue(Pos.CENTER_LEFT);
+        bottomRightBox.getChildren().addAll(bottomRight);
+        bottomRightBox.alignmentProperty().setValue(Pos.CENTER_RIGHT);
+        bottomBox.getChildren().addAll(bottomLeftBox,bottomRightBox);
+
+        bp.setTop(topBox);
+        bp.setCenter(centerPane);
+        bp.setBottom(bottomBox);
+
+        pane.getChildren().addAll(bp);
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
