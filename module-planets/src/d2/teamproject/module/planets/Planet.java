24125a121372c07704994018976535f7ae16e537
Joseph Groocock <frebib@gmail.com>
2016-02-14 17:20:05 +0000

    Planets: Begin implementation
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@93 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

---
+++ module-planets/src/d2/teamproject/module/planets/Planet.java
@@ -0,0 +1,37 @@
+package d2.teamproject.module.planets;
+
+import com.eclipsesource.json.JsonObject;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Comparator;
+
+public class Planet implements Comparable<Planet> {
+    private String name;
+    private long mass, diameter, distToSun;
+    private Comparator<Planet> cmp;
+
+    @Override
+    public int compareTo(Planet planet) {
+        return cmp.compare(this, planet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getMass() {
+        return mass;
+    }
+
+    public long getDiameter() {
+        return diameter;
+    }
+
+    public long getDistToSun() {
+        return distToSun;
+    }
+
+    public Planet loadFromFile(JsonObject obj) {
+        throw new NotImplementedException();
+    }
+}
--- module-planets/src/d2/teamproject/module/planets/PlanetModule.java
+++ module-planets/src/d2/teamproject/module/planets/PlanetModule.java
@@ -1,9 +1,23 @@
 package d2.teamproject.module.planets;
 
 import d2.teamproject.module.JsonModule;
+import d2.teamproject.algorithm.sorting.QuickSortStream;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class PlanetModule extends JsonModule {
+    private QuickSortStream<Planet> sort;
+    private List<Planet> planets;
+
+    private Comparator<Planet> planetCompare;
+
     public PlanetModule() {
+        // TODO: Load planets
+        planets = new ArrayList<Planet>(8);
+        sort = new QuickSortStream<>(planets);
+
         // TODO: Implement planet sorting
     }
 }
