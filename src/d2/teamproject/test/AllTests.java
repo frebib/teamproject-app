48e3261daad79466a04dc979c4b2051df7018f4d
Otonye Bestman <oerebestman@yahoo.com>
2016-02-15 20:19:56 +0000

    I used a JUnit suite to test run all the test classes at once
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@102 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

---
+++ src/d2/teamproject/test/AllTests.java
@@ -0,0 +1,11 @@
+package d2.teamproject.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BubbleSortTest.class, QuickSortTest.class })
+public class AllTests {
+
+}
