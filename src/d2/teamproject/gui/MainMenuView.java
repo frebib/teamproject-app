24da3b47e1510635b4f1a68d32c439e97cdbcf2a
Parth Chandratreya <pac442@bham.ac.uk>
2016-02-15 12:44:55 +0000

    Removed redundant event handlers and deprecated code
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@96 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/gui/MainMenuView.java
+++ src/d2/teamproject/gui/MainMenuView.java
@@ -13,7 +13,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.util.List;
 
 public class MainMenuView {
@@ -66,23 +65,15 @@ public class MainMenuView {
         imgView.setEffect(focus);
 
         /* Events */
-        t.setOnMouseEntered(e -> {
+        sp.setOnMouseEntered(e -> {
             imgView.setEffect(blur);
             t.setVisible(true);
         });
-        t.setOnMouseExited(e -> {
+        sp.setOnMouseExited(e -> {
             imgView.setEffect(focus);
             t.setVisible(false);
         });
-        imgView.setOnMouseEntered(e -> {
-            imgView.setEffect(blur);
-            t.setVisible(true);
-        });
-        imgView.setOnMouseExited(e -> {
-            imgView.setEffect(focus);
-            t.setVisible(false);
-        });
-        imgView.setOnMouseClicked(e -> {
+        sp.setOnMouseClicked(e -> {
             System.out.println("Clicked " + t.getText());
             VisView view = new VisView(vis.getView().getPane(), e1 -> stage.setScene(menu));
             stage.setScene(view.getScene());
@@ -90,46 +81,4 @@ public class MainMenuView {
         return sp;
     }
 
-    /*
-    @Deprecated
-    public StackPane BasicScene() {
-        // Border pane implementation - Holds all the different sections
-        BorderPane bp = new BorderPane();
-        bp.setPadding(new Insets(10, 20, 10, 20));
-
-        // Back button implementation - Used to go back to the main menu
-        Button backButton = new Button("BACK");
-        HBox topBox = new HBox();
-        topBox.getChildren().addAll(backButton);
-        backButton.setOnMouseClicked(e -> {
-//            mMenu.setVisible(true);
-//            pane.setVisible(false);
-        });
-
-        // Center pane implementation - Used to visualise the algorithms
-        StackPane centerPane = new StackPane();
-        // CALL SPECIFIC CLASS HERE
-
-        // Help button implementation - Used to initialise the tutorial mode
-        Button helpButton = new Button("HELP");
-        HBox helpButtonBox = new HBox();
-        helpButtonBox.getChildren().addAll(helpButton);
-        helpButtonBox.alignmentProperty().setValue(Pos.CENTER_LEFT);
-
-        // Tick button implementation - Use to check the users' work
-        Button tickButton = new Button("TICK");
-        HBox tickButtonBox = new HBox();
-        tickButtonBox.getChildren().addAll(tickButton);
-        tickButtonBox.alignmentProperty().setValue(Pos.CENTER_RIGHT);
-
-        // Pane to hold the bottom gui elements
-        StackPane bottomBox = new StackPane();
-        bottomBox.getChildren().addAll(helpButtonBox, tickButtonBox);
-
-        bp.setTop(topBox);
-        bp.setCenter(centerPane);
-        bp.setBottom(bottomBox);
-        pane.getChildren().addAll(bp);
-        return pane;
-    } */
 }
