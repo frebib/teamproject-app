09e988d7ed20ae18dcd8b0d7a3fffc063181f71e
Joseph Groocock <frebib@gmail.com>
2016-02-16 12:51:01 +0000

    PlanetController: Add primitive Comparer chooser
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@115 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- module-planets/src/d2/teamproject/module/planets/PlanetController.java
+++ module-planets/src/d2/teamproject/module/planets/PlanetController.java
@@ -13,15 +13,20 @@ import java.util.List;
 import java.util.Map;
 
 public class PlanetController extends JsonController {
+
+    public enum PlanetSort {
+        DIST_TO_SUN,
+        DIAMETER,
+        MASS
+    }
+
     private QuickSortStream<Planet> sort;
     private List<Planet> planets;
 
     private Comparator<Planet> planetCompare;
 
     public PlanetController() {
-        // TODO: Load planets
-        planets = new ArrayList<Planet>(8);
-        sort = new QuickSortStream<>(planets, (o1, o2) -> Float.compare(o1.getDistToSun(), o2.getDistToSun()));
+        sort = new QuickSortStream<>(planets, getPlanetCompare(PlanetSort.DIST_TO_SUN));
 
         // TODO: Implement planet sorting
     }
@@ -38,5 +43,18 @@ public class PlanetController extends JsonController {
             planets.add(Planet.loadFromJson(pData.asObject()));
 
         planets.stream().forEach(System.out::println);
+    }
+
+    public Comparator<Planet> getPlanetCompare(PlanetSort sortBy) {
+        // Is ugly. Needs to be cleaned up
+        switch (sortBy) {
+            case DIST_TO_SUN:
+                return (p1, p2) -> Float.compare(p1.getDistToSun(), p2.getDistToSun());
+            case DIAMETER:
+                return (p1, p2) -> Float.compare(p1.getDiameter(), p2.getDistToSun());
+            case MASS:
+                return (p1, p2) -> Float.compare(p1.getMass(), p2.getMass());
+        }
+        return null; // Java, both you and I know execution will never reach here so shut the F#!$ up
     }
 }
