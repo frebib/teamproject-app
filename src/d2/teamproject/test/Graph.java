a6df7092f00cfd36b7835b2af4681aaa0de9d174
Otonye Bestman <oerebestman@yahoo.com>
2016-02-17 14:54:23 +0000

    New updates
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@155 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/test/Graph.java
+++ src/d2/teamproject/test/Graph.java
@@ -9,18 +9,14 @@ public class Graph<A> {
 	// Keep the implementation of maps open, by using the Map interface:
 	private Map<A, Node<A>> nodes;
 
-	/**
-	 * Constructs a new empty {@link Graph}
-	 */
 	public Graph() {
 		nodes = new LinkedHashMap<A, Node<A>>();
 	}
 
 	/**
-	 * Find or create {@link Node} with a given contents c
 	 *
-	 * @param c Contents of the {@link Node} to find
-	 * @return A {@link Node} with contents c
+	 * @param c 
+	 * @return A 
 	 */
 	public Node<A> nodeWith(A c) {
 		Node<A> node; // Deliberately uninitialized.
@@ -33,11 +29,6 @@ public class Graph<A> {
 		return node;
 	}
 
-	/**
-	 * Gets {@link Node}{@code s} in the {@link Graph}
-	 *
-	 * @return {@link Node}{@code s} from the {@link Graph}
-	 */
 	public Map<A, Node<A>> getNodes() {
 		return nodes;
 	}
