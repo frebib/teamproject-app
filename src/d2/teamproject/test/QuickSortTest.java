ee507384f8bd0d1417ef324c8e52fef9db396965
Joseph Groocock <frebib@gmail.com>
2016-02-17 13:59:19 +0000

    QuickSortTest: Actually test sort
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@146 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/test/QuickSortTest.java
+++ src/d2/teamproject/test/QuickSortTest.java
@@ -1,34 +1,34 @@
 package d2.teamproject.test;
 
 import d2.teamproject.algorithm.sorting.QuickSortStream;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class QuickSortTest {
 
-	@Test
-	public void test() {
-		List<Integer> list= new ArrayList<>();
-    	list.add(2);
-    	list.add(6);
-    	list.add(3);
-    	list.add(8);
-    	list.add(9);
-    	list.add(4);
-    	list.add(1);
-    	
-        QuickSortStream<Integer> sort = new QuickSortStream<>(list, Integer::compare);
-        sort.initialise();
-        String result = sort.getSortedList().stream().map(Object::toString).collect(Collectors.joining(" "));
-        assertArrayEquals("1 2 3 4 6 8 9", result);
-        }
+    @Test
+    public void quickSortTest() {
+        List<Integer> sortedList = IntStream.range(0, 20).boxed().collect(Collectors.toList());
+        List<Integer> shuffledList = new ArrayList<>(sortedList);
+        Collections.shuffle(shuffledList);
 
-	private void assertArrayEquals(String string, String result) {
-		// TODO Auto-generated method stub
-		
-	}
+        QuickSortStream<Integer> sorter = new QuickSortStream<>(shuffledList, Integer::compare);
+        sorter.initialise();
+        Assert.assertTrue(listEqual(sortedList, sorter.getSortedList()));
+    }
 
+    private <E extends Comparable<E>> boolean listEqual(List<E> a, List<E> b) {
+        if (a.size() != b.size())
+            return false;
+        for (int i = 0; i < a.size(); i++)
+            if (!a.get(i).equals(b.get(i)))
+                return false;
+        return true;
+    }
 }
