26f33f8d87d1cc0bd073f1a8379eadd8253022d3
Luke Taher <luketaher@gmail.com>
2016-02-17 13:44:31 +0000

    Pair: Created class
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@144 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

---
+++ src/d2/teamproject/tutorial/Pair.java
@@ -0,0 +1,24 @@
+package d2.teamproject.tutorial;
+
+public class Pair<A, B> {
+
+    private A a;
+    private B b;
+
+    public Pair(A a, B b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getFirst()
+    {
+        return a;
+    }
+
+    public B getSecond()
+    {
+        return b;
+    }
+
+}
