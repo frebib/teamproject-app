dc2fa959064d8b3050ce4beb62c845925484fbd6
Gulraj Bariah <gulrajb@hotmail.com>
2016-02-17 14:27:52 +0000

    InstructionSet has get methods added
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@152 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/tutorial/InstructionSet.java
+++ src/d2/teamproject/tutorial/InstructionSet.java
@@ -4,5 +4,32 @@ import java.util.ArrayList;
 
 public class InstructionSet {
     private ArrayList<Instruction> set;
-    //TODO - get next, prev, count, isNext, isPrev
+    private int tracker;
+
+    public Instruction getNext() {
+        //get the nexdt element in the array
+        tracker++;
+        return set.get(tracker+1);
+    }
+
+    public Instruction getPrev() {
+        //get the previous element in the array
+        tracker--;
+        return set.get(tracker-1);
+    }
+
+    public int count() {
+        //the array element we're on
+        return set.get();
+    }
+
+    public boolean hasNext() {
+        //check if we are at the end of the array
+        return tracker < set.size();
+    }
+
+    public boolean hasPrev() {
+        //to check if we are at the beginning of the array
+        return tracker > 0;
+    }
 }
