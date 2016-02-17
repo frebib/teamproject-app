eb3433fc7fbb6973af44e01e5e2dec75e10cb324
Luke Taher <luketaher@gmail.com>
2016-02-17 13:45:15 +0000

    Instruction: Created methods for creation and manipulation of instructions
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@145 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/tutorial/Instruction.java
+++ src/d2/teamproject/tutorial/Instruction.java
@@ -3,6 +3,39 @@ package d2.teamproject.tutorial;
 import java.util.ArrayList;
 
 public class Instruction {
-    private ArrayList<Integer> items;
+    private String title;
+    private String desc;
+    private ArrayList<Pair<Integer, Integer>> visChanges;
+
+    public Instruction(String title, String desc, ArrayList<Pair<Integer, Integer>> visChanges)
+    {
+        this.title = title;
+        this.desc = desc;
+        this.visChanges = visChanges;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getDesc()
+    {
+        return desc;
+    }
+
+    public Boolean isNextVisChange()
+    {
+        return visChanges.size() > 0;
+    }
+
+    public Pair<Integer, Integer> getNextVisChange()
+    {
+        Pair<Integer, Integer> next = visChanges.get(visChanges.size() - 1);
+        visChanges.remove(visChanges.size() - 1);
+        return next;
+    }
+
+
 
 }
