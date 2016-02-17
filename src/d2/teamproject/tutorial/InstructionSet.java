b4a8c039a95bdaeb90a14f9f98ec587edae1c823
Luke Taher <luketaher@gmail.com>
2016-02-17 13:11:08 +0000

    InstructionSet: Created class
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@142 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

---
+++ src/d2/teamproject/tutorial/InstructionSet.java
@@ -0,0 +1,8 @@
+package d2.teamproject.tutorial;
+
+import java.util.ArrayList;
+
+public class InstructionSet {
+    private ArrayList<Instruction> set;
+    //TODO - get next, prev, count, isNext, isPrev
+}
