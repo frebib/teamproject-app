852413c7aaf5f0deebcb694db50ae059b4875274
Otonye Bestman <oerebestman@yahoo.com>
2016-03-08 22:33:50 +0000

    Bubble sort stream finito
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@351 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/algorithm/sorting/BubbleSortStream.java
+++ src/d2/teamproject/algorithm/sorting/BubbleSortStream.java
@@ -14,111 +14,101 @@ import java.util.stream.Collectors;
  */
 
 public class BubbleSortStream<E extends Comparable<E>> implements AlgoStream<SortState<E>> {
-	 private List<E> list;
-	    private Comparator<E> comparator;
+	private List<E> list;
+	private Comparator<E> comparator;
 
-	    private int stateIndex = 0;
-	    private final List<SortState<E>> states;
+	private int stateIndex = 0;
+	private final List<SortState<E>> states;
+	boolean swap;
+	private ListSortState<E> lastListState;
 
-	    private ListSortState<E> lastListState;
+	/**
+	 * Creates a new QuickSortStream provided with a list to sort and create
+	 * states for
+	 * 
+	 * @param list
+	 *            a list to sort
+	 * @param comparator
+	 *            comparison object used to sort list
+	 */
+	public BubbleSortStream(List<E> list, Comparator<E> comparator) {
+		this.list = new ArrayList<>(list);
+		this.comparator = comparator;
+		states = new ArrayList<>();
+	}
 
-	    /**
-	     * Creates a new QuickSortStream provided with a list to sort and create states for
-	     * @param list       a list to sort
-	     * @param comparator comparison object used to sort list
-	     */
-	    public BubbleSortStream(List<E> list, Comparator<E> comparator) {
-	        this.list = new ArrayList<>(list);
-	        this.comparator = comparator;
-	        states = new ArrayList<>();
-	    }
-	
-	public List<E> sort(List<E> items){
-		for(int i = 0; i < items.size() - 1; i++){
-			for(int k = items.size() - 1; k > i; k--){
-				//items.get(i).compareTo(items.get(k)) > 0
-				if(comparator.compare(items.get(i), items.get(k))>0){
+	public List<E> sort(List<E> items) {
+		for (int i = 0; i < items.size() - 1; i++) {
+			for (int k = items.size() - 1; k > i; k--) {
+				swap = items.get(i).compareTo(items.get(k)) > 0;
+				if (swap) {
 					E store = items.get(i);
 					items.set(i, items.get(k));
 					items.set(k, store);
+					lastListState = new ListSortState<>(list);
+					states.add(lastListState);
 				}
+				states.add(new CompareSortState<>(lastListState, 0, 0, list.size() - 1, i, k, swap));
+
 			}
 		}
 		return items;
 	}
-	/*public static void main(String[] args) {
-     //List<Integer> list = new Random().ints(20, 0, 100).boxed().collect(Collectors.toList());
-		List<Integer> list= new ArrayList<>();
- 	list.add(2);
- 	list.add(6);
- 	list.add(3);
- 	list.add(8);
- 	list.add(9);
- 	list.add(4);
- 	list.add(1);
- 	
-     System.out.println(list.stream().map(Object::toString).collect(Collectors.joining(", ")));
-     System.out.println(new BubbleSort<Integer>()
-     		.sort(list)
-     		.stream()
-     		.map(Object::toString)
-     		.collect(Collectors.joining(", ")));
- }*/
-	
-	 public List<E> getSortedList() {
-	        return list;
-	    }
-	
-		@Override
-	public AlgoStream<SortState<E>> initialise() {
-		// TODO Auto-generated method stub
-		  if (list == null || list.size() < 1)
-	            return this;
 
-	        lastListState = new ListSortState<>(list);
-	        states.add(lastListState);
-	        sort(list);
-	        states.add(new ListSortState<>(list, true));
-	        return this;
+	public List<E> getSortedList() {
+		return list;
 	}
-		  @Override
-		    public SortState<E> getNext() {
-		        if (hasNext())
-		            return states.get(++stateIndex);
-		        return null;
-		    }
 
-		    @Override
-		    public SortState<E> getPrevious() {
-		        if (hasPrevious())
-		            return states.get(--stateIndex);
-		        return null;
-		    }
+	@Override
+	public AlgoStream<SortState<E>> initialise() {
+		if (list == null || list.size() < 1)
+			return this;
 
-		    @Override
-		    public SortState<E> getNth(int n) {
-		        if (hasNth(n))
-		            return states.get(n);
-		        return null;
-		    }
+		lastListState = new ListSortState<>(list);
+		states.add(lastListState);
+		sort(list);
+		states.add(new ListSortState<>(list, true));
+		return this;
+	}
 
-		    @Override
-		    public boolean hasNext() {
-		        return stateIndex + 1 < states.size();
-		    }
+	@Override
+	public SortState<E> getNext() {
+		if (hasNext())
+			return states.get(++stateIndex);
+		return null;
+	}
 
-		    @Override
-		    public boolean hasPrevious() {
-		        return stateIndex > 0;
-		    }
+	@Override
+	public SortState<E> getPrevious() {
+		if (hasPrevious())
+			return states.get(--stateIndex);
+		return null;
+	}
 
-		    @Override
-		    public boolean hasNth(int n) {
-		        return n >= 0 && n < states.size();
-		    }
+	@Override
+	public SortState<E> getNth(int n) {
+		if (hasNth(n))
+			return states.get(n);
+		return null;
+	}
 
-		    @Override
-		    public List<SortState<E>> getAll() {
-		        return states;
-		    }
+	@Override
+	public boolean hasNext() {
+		return stateIndex + 1 < states.size();
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		return stateIndex > 0;
+	}
+
+	@Override
+	public boolean hasNth(int n) {
+		return n >= 0 && n < states.size();
+	}
+
+	@Override
+	public List<SortState<E>> getAll() {
+		return states;
+	}
 }
