26ecdf6edd0cb6a462c6d5bc5b3506ef4943c0ae
Joseph Groocock <frebib@gmail.com>
2016-03-08 00:20:50 +0000

    Log: Print brackets around date
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@326 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- src/d2/teamproject/util/Log.java
+++ src/d2/teamproject/util/Log.java
@@ -30,7 +30,7 @@ public class Log implements Thread.UncaughtExceptionHandler {
             new File(path).getParentFile().mkdirs();
 
             System.setProperty("java.util.logging.SimpleFormatter.format",
-                    "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS [%4$s] %5$s%6$s%n");
+                    "[%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS] [%4$s] %5$s%6$s%n");
             fh = new FileHandler(path);
             fh.setFormatter(new SimpleFormatter());
 
