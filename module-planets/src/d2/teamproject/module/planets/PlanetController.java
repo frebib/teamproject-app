bf33badfcac8e17640f5a573734782df59e229a6
Joseph Groocock <frebib@gmail.com>
2016-03-08 11:37:32 +0000

    PlanetSort: Move to it's own class file
    git-svn-id: https://codex.cs.bham.ac.uk/svn/team-project/D2@337 cfd7d107-c69a-4b07-9ecf-543bb1cfc744

--- module-planets/src/d2/teamproject/module/planets/PlanetController.java
+++ module-planets/src/d2/teamproject/module/planets/PlanetController.java
@@ -8,6 +8,7 @@ import d2.teamproject.algorithm.sorting.SortState;
 import d2.teamproject.module.BaseView;
 import d2.teamproject.module.JsonController;
 import d2.teamproject.module.ModuleLoader;
+import d2.teamproject.module.planets.gfx.PlanetSort;
 import javafx.scene.image.Image;
 
 import java.io.BufferedInputStream;
@@ -23,54 +24,6 @@ import static d2.teamproject.PARTH.LOG;
 
 public class PlanetController extends JsonController {
 
-    public static class PlanetSort<T extends Comparable<T>> implements Comparator<Planet> {
-        private enum Dir {
-            ASCENDING(1),
-            DESCENDING(-1);
-
-            int polarity;
-            Dir(int i) {
-                polarity = i;
-            }
-
-            @Override
-            public String toString() {
-                return this.name().charAt(0) +
-                        this.name().substring(1).toLowerCase();
-            }
-        }
-
-        public static final PlanetSort<Float>
-                DIST_TO_SUN = new PlanetSort<>(Planet::getDistToSun),
-                ROTATE_TIME = new PlanetSort<>(Planet::getRotationTime),
-                DIAMETER = new PlanetSort<>(Planet::getDiameter),
-                MASS = new PlanetSort<>(Planet::getMass);
-
-        private Function<Planet, T> fn;
-        private Dir dir;
-
-        private PlanetSort(Function<Planet, T> fn) {
-            this(fn, Dir.ASCENDING);
-        }
-        private PlanetSort(Function<Planet, T> fn, Dir dir) {
-            this.fn = fn;
-            this.dir = dir;
-        }
-
-        @Override
-        public int compare(Planet p1, Planet p2) {
-            return fn.apply(p1).compareTo(fn.apply(p2)) * dir.polarity;
-        }
-        public String getDirection() {
-            return dir.toString();
-        }
-        public PlanetSort<T> setAscending() {
-            return new PlanetSort<>(this.fn, Dir.ASCENDING);
-        }
-        public PlanetSort<T> setDescending() {
-            return new PlanetSort<>(this.fn, Dir.DESCENDING);
-        }
-    }
 
     private PlanetView view;
 
